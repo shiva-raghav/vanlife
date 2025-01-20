540e6a7dbc1c69c93856330909fd41d5aee97b76
Raghav Shiva <shiva.raghav@gmail.com>
2025-01-20 12:43:17 -0500

    1.) Added media queries for different screen sizes. 2.)Added barebones of Van Detail Page

---
+++ pages/vans/VanDetail.jsx
@@ -0,0 +1,9 @@
+export default function VanDetail() {
+  return (
+    <>
+      <div className="container product-container">
+        <h1>This is Van Detail Page</h1>
+      </div>
+    </>
+  );
+}
