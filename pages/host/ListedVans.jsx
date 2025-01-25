47e5dee76360d47dc13b1025753cb9b6e3d7175c
Raghav Shiva <shiva.raghav@gmail.com>
2025-01-24 20:53:12 -0500

    updated routes and added pages for host

--- pages/host/ListedVans.jsx
+++
@@ -1,9 +0,0 @@
-export default function ListedVans() {
-  return (
-    <>
-      <div className="listedVans">
-        <h1>ListedVans goes here ...</h1>
-      </div>
-    </>
-  );
-}
---
+++ pages/host/index.jsx
@@ -0,0 +1,15 @@
+import Dashboard from "./Dashboard";
+import Income from "./Income";
+import Vans from "./Vans";
+import Van from "./Van";
+import Reviews from "./Reviews";
+
+const Host = {};
+
+Host.Dashboard = Dashboard;
+Host.Income = Income;
+Host.Vans = Vans;
+Host.Van = Van;
+Host.Reviews = Reviews;
+
+export default Host;
