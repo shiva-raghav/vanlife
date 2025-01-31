c185751dccde71537056749ea4a39af910e164dd
Raghav Shiva <shiva.raghav@gmail.com>
2025-01-30 22:32:23 -0500

    Added 404 page, Added filters on VansCategory page, updated routes and updated styles

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "../components/Layout";
 import Home from "../pages/home/Home";
 import About from "../pages/about/About";
+import NotFound from "../pages/NotFound";
 import VansCategory from "../pages/vans/VansCategory";
 import VanDetail from "../pages/vans/VanDetail";
 import Host from "../pages/host/index";
@@ -12,11 +13,12 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route element={<Layout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/vans" element={<VansCategory />} />
-            <Route path="/vans/:id" element={<VanDetail />} />
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="about" element={<About />} />
+            <Route path="vans" element={<VansCategory />} />
+            <Route path="vans/:id" element={<VanDetail />} />
+
             <Route path="/host" element={<HostLayout />}>
               <Route index element={<Host.Dashboard />} />
               <Route path="income" element={<Host.Income />} />
@@ -24,6 +26,7 @@ function App() {
               <Route path="vans/:id" element={<Host.Van />} />
               <Route path="reviews" element={<Host.Reviews />} />
             </Route>
+            <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
       </BrowserRouter>
